a285fe6aa5b2d132f3e92a7b48cb2b176b6a0d51
Adi <adityasharma08official@gmail.com>
2024-07-01 13:30:43 +0530

    Updated logout function

--- controllers/user.js
+++ controllers/user.js
@@ -76,7 +76,7 @@ exports.logout = (req, res) => {
     .status(200)
     .cookie("token", "", {
       expires: new Date(Date.now()),
-      sameSite: process.env.NODE_ENV === "Development" ? "lax" : "true",
+      sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
       secure: process.env.NODE_ENV === "Development" ? false : true,
     })
     .json({
